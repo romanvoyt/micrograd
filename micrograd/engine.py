c2c1f698167f0367a757abfea9b0e6a9dba3d8e0
romanvoyt <romanvoitetckii@gmail.com>
2022-11-10 15:27:49 +0300

    some indentation bugs are fixed

--- micrograd/engine.py
+++ micrograd/engine.py
@@ -43,34 +43,26 @@ class Value:
 
         return out
 
+    def __neg__(self):
+        return self * -1
 
-def __neg__(self):
-    return self * -1
+    def __radd__(self, other):
+        return other + self
 
+    def __sub__(self, other):
+        return self + (-other)
 
-def __radd__(self, other):
-    return self + other
+    def __rsub__(self, other):
+        return other + (-self)
 
+    def __rmul__(self, other):
+        return other + (-self)
 
-def __sub__(self, other):
-    return self + (-other)
+    def __truediv__(self, other):
+        return self * other ** -1
 
+    def __rtruediv__(self, other):
+        return other * self ** -1
 
-def __rsub__(self, other):
-    return other + (-self)
-
-
-def __rmul__(self, other):
-    return other + (-self)
-
-
-def __truediv__(self, other):
-    return self * other ** -1
-
-
-def __rtruediv__(self, other):
-    return other * self ** -1
-
-
-def __repr__(self):
-    return f'Value(data={self.data}, grad={self.grad:.4f})'
+    def __repr__(self):
+        return f'Value(data={self.data}, grad={self.grad:.4f})'
