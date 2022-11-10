eb5860256baa1f2a4cb5cda992b9f8730e90579d
romanvoyt <romanvoitetckii@gmail.com>
2022-11-10 11:38:42 +0300

    basic engine.py functionality is added.

---
+++ micrograd/engine.py
@@ -0,0 +1,47 @@
+class Value:
+    """ Хранит значение скаляра и его градиентов """
+    def __init__(self, data, _children=(), _op=''):
+        self.data = data
+        self.grad = 0
+        self._backward = lambda: None
+        self._prev = set(_children)
+        self._op = _op
+
+    def __add__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data + other.data, (self, other), '+')
+
+        def _backward():
+            self.grad += out.grad
+            other.grad += out.grad
+
+        out._backward = _backward
+
+        return out
+
+    def __mul__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data + other.data, (self, other), '+')
+
+        def _backward():
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
+
+        out._backward = _backward
+
+        return out
+
+    def __pow__(self, other):
+        assert isinstance(other, (int, float))
+        out = Value(self.data**other, (self,), f'**{other}')
+
+        def _backward():
+            self.grad += (other * self.data**(other-1)) * out.grad
+
+        out._backward = _backward
+
+        return out
+
+    def __repr__(self):
+        return f'Value(data={self.data}, grad={self.grad:.4f})'
+
