b2f77115552f8b637f859e4f4ea6dd2f9c982aad
romanvoyt <romanvoitetckii@gmail.com>
2022-11-10 15:15:29 +0300

    New ops are added (eg. reverse division, subtraction etc)

--- micrograd/engine.py
+++ micrograd/engine.py
@@ -1,5 +1,6 @@
 class Value:
     """ Хранит значение скаляра и его градиентов """
+
     def __init__(self, data, _children=(), _op=''):
         self.data = data
         self.grad = 0
@@ -21,7 +22,7 @@ class Value:
 
     def __mul__(self, other):
         other = other if isinstance(other, Value) else Value(other)
-        out = Value(self.data + other.data, (self, other), '+')
+        out = Value(self.data * other.data, (self, other), '*')
 
         def _backward():
             self.grad += other.data * out.grad
@@ -33,15 +34,43 @@ class Value:
 
     def __pow__(self, other):
         assert isinstance(other, (int, float))
-        out = Value(self.data**other, (self,), f'**{other}')
+        out = Value(self.data ** other, (self,), f'**{other}')
 
         def _backward():
-            self.grad += (other * self.data**(other-1)) * out.grad
+            self.grad += (other * self.data ** (other - 1)) * out.grad
 
         out._backward = _backward
 
         return out
 
-    def __repr__(self):
-        return f'Value(data={self.data}, grad={self.grad:.4f})'
 
+def __neg__(self):
+    return self * -1
+
+
+def __radd__(self, other):
+    return self + other
+
+
+def __sub__(self, other):
+    return self + (-other)
+
+
+def __rsub__(self, other):
+    return other + (-self)
+
+
+def __rmul__(self, other):
+    return other + (-self)
+
+
+def __truediv__(self, other):
+    return self * other ** -1
+
+
+def __rtruediv__(self, other):
+    return other * self ** -1
+
+
+def __repr__(self):
+    return f'Value(data={self.data}, grad={self.grad:.4f})'
