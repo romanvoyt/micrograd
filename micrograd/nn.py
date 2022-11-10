dc2358f7feac27d67ca9a98a00e2a1f0eec7ece6
romanvoyt <romanvoitetckii@gmail.com>
2022-11-10 16:37:23 +0300

    nn module is done

---
+++ micrograd/nn.py
@@ -0,0 +1,61 @@
+import random
+from micrograd.engine import Value
+
+
+class Module:
+    def zero_grad(self):
+        for p in self.parameters():
+            p.grad = 0
+
+    def parameters(self):
+        return []
+
+
+class Neuron(Module):
+    def __init__(self, nin, non_linear=True):
+        self.w = [Value(random.uniform(-1, 1)) for _ in range(nin)]
+        self.b = Value(0)
+        self.non_linear = non_linear
+
+    def __call__(self, x):
+        act = sum((wi, xi for wi, xi in zip(self.w, x)), self.b)
+        return act.relu() if self.non_linear else act
+
+    def parameters(self):
+        return self.w + [self.b]
+
+    def __repr__(self):
+        return f"{'ReLU' if self.non_linear else 'Linear'}Neuron({len(self.w)})"
+
+
+class Layer(Module):
+    def __init__(self, n_in, n_out, **kwargs):
+        self.neurons = [Neuron(n_in, **kwargs) for _ in range(n_out)]
+
+    def __call__(self, x):
+        out = [n(x) for n in self.neurons]
+        return out[0] if len(out) == 1 else out
+
+    def parameters(self):
+        return [p for n in self.neurons for p in n.parameters()]
+
+    def __repr__(self):
+        return f"Layer of [{', '.join(str(n) for n in self.neurons)}"
+
+
+class MLP(Module):
+    def __init__(self, n_in, n_outs):
+        sz = [n_in] + n_outs
+        self.layers = [Layer(sz[i], sz[i + 1], non_linear=i != len(n_outs) - 1)
+                       for i in range(len(n_outs))]
+
+    def __call__(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
+
+    def parameters(self):
+        return [p for layer in self.layers for p in layer.parameters()]
+
+    def __repr__(self):
+        return f"MLP of [{', '.join(str(layer) for layer in self.layers)}]"
